87aa7db3781a18a484088d19f4a3933d91128300
rodrigo.araujo13 <rodrigo.araujo13@f0a913d4-fee3-9a03-34f4-59f46033e27e>
2014-04-26 19:37:58 +0000

    Começando

---
+++ BRadio/src/bradio/BRadio.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bradio;
+
+import insidefx.undecorator.Undecorator;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class BRadio extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+
+        Undecorator undecorator = new Undecorator(stage, (Region) root);
+        undecorator.getStylesheets().add("skin/undecorator.css");
+        Scene scene = new Scene(undecorator);
+        //Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
